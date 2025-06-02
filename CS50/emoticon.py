1ab83577699d93fdcecd7e16fda2a92a690ea750
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-02 20:27:50 +0000

    Mon, Jun 2, 2025, 3:27 PM -05:00

--- CS50/emoticon.py
+++ CS50/emoticon.py
@@ -1,9 +1,10 @@
 emo = "v.v"
 
 def main():
-    print("Anyone there?")
+    say("Anyone there?")
 
 def say(phrase):
     print(phrase + " " + emo)
 
+# "phrase" will make the program run the the string we typed
 main()
