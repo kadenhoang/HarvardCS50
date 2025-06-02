1212208b0df0d85f1985daa80a2f3bb7a909a4a6
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-02 19:40:46 +0000

    Mon, Jun 2, 2025, 2:40 PM -05:00

---
+++ CS50/guess.py
@@ -0,0 +1,17 @@
+#define a function or create a class
+def getGuess():
+    guess = int(input("make a guess: "))
+    return guess
+
+#define the main()
+def main():
+    guess = getGuess()
+#put : after if - the next line should space inward
+#means the next line is in the if function
+    if guess == 10:
+        print("correct")
+    else:
+        print("try again")
+
+# make this line so the function works
+main()
