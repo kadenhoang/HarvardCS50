9e87e1eb34764f82e92972478b57f11c25b3bf8d
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-02 20:03:25 +0000

    Mon, Jun 2, 2025, 3:03 PM -05:00

--- CS50/greeting.py
+++ CS50/greeting.py
@@ -2,9 +2,9 @@ def greet(input):
     if "hello" in input:
         return "Hello, my man"
     else:
-        return "I dont know what you mean"
+        return "I'm not sure what you mean"
 
-greeting = greet("brother")
+greeting = greet("Hello, My Guy")
 print(greeting,", hehe")
 
 # use return instead of print let us modify the answer for later use
