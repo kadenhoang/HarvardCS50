575cd06cb04cafc3b750fbc9ec3f918a83515501
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-02 20:47:38 +0000

    Mon, Jun 2, 2025, 3:47 PM -05:00

--- CS50/nameCleanup.py
+++ CS50/nameCleanup.py
@@ -1,8 +1,8 @@
 SHOWS = [
-    " Avatar: what is that?
-    "pokemon"
-    "   ben 10   "
-    "Stranger thing"
+    " Avatar: what is that?",
+    "pokemon",
+    "   ben 10   ",
+    "Stranger thing",
 ]
 
 def main():
