3266642bab573c810eb64fcb8ecf997e4e2379cd
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-02 20:33:35 +0000

    Mon, Jun 2, 2025, 3:33 PM -05:00

--- CS50/emoticon.py
+++ CS50/emoticon.py
@@ -1,8 +1,16 @@
+#this line can be access anywhere (global)
 emo = "v.v"
 
 #create a function main() that say(smt)
 def main():
+    global emo
     say("Anyone there?")
+    emo = ":D"
+    say("Supriseeeee")
+    emo = ">:()"
+    say("Wtf, you scared me!!!")
+
+
 
 #create a function that when say(smt) it print smt on demand
 def say(phrase):
