df12a7189ac1544bdeaf0b5e38d5b09cff2fcfa9
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-06 21:08:50 +0000

    Fri, Jun 6, 2025, 4:08 PM -05:00

---
+++ deep/deep.py
@@ -0,0 +1,5 @@
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything?: ")
+if answer is not "42" or "forty-two" or "forty two" :
+    print("No")
+else:
+    print("Yes")
