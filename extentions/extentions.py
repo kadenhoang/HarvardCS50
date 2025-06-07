ccec4947e19d1b6ddc171f9e4563392bee27014c
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-07 20:36:22 +0000

    Sat, Jun 7, 2025, 3:36 PM -05:00

--- extentions/extentions.py
+++ extentions/extentions.py
@@ -3,10 +3,16 @@ filename = input("File Name: ")
 if ".gif" in filename:
     print("image/gif")
 elif ".png" in filename:
-    print("image/png)
+    print("image/png")
 elif ".jpeg" in filename:
-    print("image/jpeg)
+    print("image/jpeg")
 elif ".png" in filename:
-    print("image/png)
+    print("image/png")
 elif ".pdf" in filename:
-    print("application/)
+    print("application/type")
+elif ".zip" in filename:
+    print("application/type")
+elif ".txt" in filename:
+    print("text/plain")
+else:
+    print("application/octet-stream")
