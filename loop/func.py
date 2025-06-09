7bf4dc4b39427b39ccab00e6fb312d15882b75d1
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-09 21:14:59 +0000

    Mon, Jun 9, 2025, 4:14 PM -05:00

--- loop/func.py
+++ loop/func.py
@@ -3,14 +3,16 @@ def main():
     meow(numbers)
 
 def get_num():
+    # while True means always true
     while True:
         n = int(input("How many Meow?: "))
-            if n > 0:
-                break
+        if n > 0:
+            break
     return n
 
+# create a fuction meow with paremeter(n)(just a name)
 def meow(n):
-    for _ in range(n)
+    for _ in range(n):
         print("Meow")
 
 main()
