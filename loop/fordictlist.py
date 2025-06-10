b460f8c29cf4e9817968db4c37863530a8cdb824
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-10 20:43:22 +0000

    Tue, Jun 10, 2025, 3:43 PM -05:00

--- loop/fordictlist.py
+++ loop/fordictlist.py
@@ -1,12 +1,12 @@
 #use [] to create a list
-#then use {} to create a dictionary
+#then use {} to create a dictionaries
+#=> create a list of dictionaries
 
 
 students = [
-    {"Vu":"Vietnam":"Leo",
-     "Hero":"Tanzania":"Scorpion",
-     "Jordan":"Malaysia":"Dragon",
-     "Peter":"America":None}
+    {"Vu":"Vietnam", "Sign":"Leo"},
+     {"Hero":"Tanzania","Sign":"Scorpion"},
+     {"Peter":"America","Sign": None}
 ]
 
 for student in range(len(students)):
