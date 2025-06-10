e268ef84f52d0d28922dce405ccb4a434665a820
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-10 20:33:37 +0000

    Tue, Jun 10, 2025, 3:33 PM -05:00

--- loop/fordict.py
+++ loop/fordict.py
@@ -1,7 +1,9 @@
-
+# {} = dictionary , it allow us to associate one thing to another using ":"
+#  think of a dictionary, the first word is the keyword, the second is the definition
+# in this case, Vu is the keyword and Vietnam is what we want to know.
 
 studentsdict = {"Vu":"Vietnam", "Hashita":"India", "Hero":"Tanzania", "Jordan":"Malay",}
 
-
 for student in studentsdict:
+    #using student only print the keyword, for definition whe need [indice "student"]
     print(student, studentsdict[student],sep=", ")
--- loop/forlist.py
+++ loop/forlist.py
@@ -1,4 +1,5 @@
 #[] = list or array in jv
+# indice i automatically set to 0
 # print the string base on the list
 # in this case, print meow 3 times
 for i in [0,1,2]:
