ecb8848b0e185f6bfc2473ff54b1aa3bd398acbc
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-13 21:17:32 +0000

    Fri, Jun 13, 2025, 4:17 PM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,7 +2,7 @@
 # ask user for input, assign it to camelcase
 camelcase = input("camelCase: ")
 #print camelcase without newline (end="") and without space (sep="")
-print(camelcase, end="")
+print("snake_case", end="")
 
 for letter in camelcase:
     if letter.isupper():
