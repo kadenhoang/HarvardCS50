b3e56abd79ec3765bd1caf0fa136f5c3639c011a
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-13 15:22:39 +0000

    Fri, Jun 13, 2025, 10:22 AM -05:00

---
+++ dict/dict.py
@@ -0,0 +1,19 @@
+def main():
+    # there are many way to create a dictionary
+    # 1. using curly braces
+    student = {"name": "Vu"}
+    # 2. using the dict() constructor
+    student["school"] = "UW-Stout"
+    # 3. using the update() method
+    student.update({"age": 31, "nationality": "Vietnamese"})
+    print(report(student))
+
+def report(student):
+return f"""
+name: {student["name"]}
+school:{student.get("school", "Unknown")}
+age: {student["age"]}
+nationality:{student.get("nationality", "Unknown")}
+"""
+
+main()
