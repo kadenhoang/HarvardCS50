c62b0ee92509e18ea2617c7bca5d5d719de06d70
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-13 20:36:59 +0000

    Fri, Jun 13, 2025, 3:36 PM -05:00

--- dict/dict.py
+++ dict/dict.py
@@ -5,13 +5,13 @@ def main():
     # 2. using the dict() constructor
     student["school"] = "UW-Stout"
     # 3. using the update() method
-    student.update({"age": 31, "nationality": "Vietnamese"})
+    student.update({"age": "31", "nationality": "Vietnamese"})
     print(report(student))
 
 def report(student):
 return f"""
 name: {student["name"]}
-school:{student.get("school", "Unknown")}
+school: {student.get("school", "Unknown")}
 age: {student["age"]}
 nationality:{student.get("nationality", "Unknown")}
 """
