026441db6951685199fe6142ae13bf3de89114ef
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-16 19:59:32 +0000

    Mon, Jun 16, 2025, 2:59 PM -05:00

--- exceptions/intexception.py
+++ exceptions/intexception.py
@@ -1,11 +1,25 @@
-# ask user for input
+def main():
+    userinput = get_int("Enter an int: ")
 
-try:
-    userinput = int(input("Enter an integer: "))
 
-# if the input is not an integer, make an exeption
-except ValueError:
-    print("That is not an integer")
+def get_int(prompt):
+    #create a loop to ask for int if input is not int
+    while True:
+    # ask user for input
+        try:
+            # use int(input())
+            # or assign with prompt and get it from the main
+            userinput = int(input(prompt))
+
+        # if the input is not an integer, make an exeption
+        except ValueError:
+            print("That is not an integer")
+        # after checking, if int = true then breakout of the loop
+        else:
+            break
+print("the integer is:", userinput)
+
+
 
 
 
