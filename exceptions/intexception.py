79df2806c573cfe4c33565dadeaca8c0fe83f283
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-16 19:51:52 +0000

    Mon, Jun 16, 2025, 2:51 PM -05:00

---
+++ exceptions/intexception.py
@@ -0,0 +1,15 @@
+# ask user for input
+
+try:
+    userinput = int(input("Enter an integer: "))
+
+# if the input is not an integer, make an exeption
+except ValueError:
+    print("That is not an integer")
+
+
+
+
+
+
+
