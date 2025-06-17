f44bc9915d47168592e9453faf0737e6acb4cec9
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-17 20:12:37 +0000

    Tue, Jun 17, 2025, 3:12 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,1 +1,26 @@
+def main():
+    fraction = input("Fraction: ")
+    print(get_fraction(fraction))
+
+
+# create a loop if user input is not a fraction
+def get_fraction(fraction)
+    while True:
+        try:
+            # ask user for the fraction
+
+            #fraction = input("Fraction:" )
+
+            #declare the parts of the fraction
+            numer, deno = fraction.split("/")
+            numer = int(numer)
+            deno = int(deno)
+
+            #print out the percentage
+            print(round(numer/deno)*100)
+            break
+
+        # if it is not a fraction then use exception
+        except (ValueError,ZeroDivisionError):
+            pass
 
