56042f589e5657a6bdea4807eea2d7e2204b24b3
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-17 20:48:58 +0000

    Tue, Jun 17, 2025, 3:48 PM -05:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,5 +1,24 @@
-# ask user to make order
-
 #create a list of dishes:
+itemlist = {"Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00}
+
+#create a loop to continue the order until done
+while True:
+    try:
+    # ask user to make order
+        item = input("Item:")
+
+        for item in itemlist:
+            if item = itemlist:
+                print(item, itemlist[item])
+            else
+
 
 #
