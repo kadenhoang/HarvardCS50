1089ad6aafbd40c740786a0cf509bad7c9bebae7
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-17 21:06:22 +0000

    Tue, Jun 17, 2025, 4:06 PM -05:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,5 +1,6 @@
 #create a list of dishes:
-itemlist = {"Baja Taco": 4.25,
+itemlist = {
+    "Baja Taco": 4.25,
     "Burrito": 7.50,
     "Bowl": 8.50,
     "Nachos": 11.00,
@@ -7,7 +8,8 @@ itemlist = {"Baja Taco": 4.25,
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
     "Taco": 3.00,
-    "Tortilla Salad": 8.00}
+    "Tortilla Salad": 8.00
+    }
 
 total = 0.0
 #create a loop to continue the order until done
