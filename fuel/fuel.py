20b43ce7238d9b96744f5dd4f9b509af6a38ef96
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-17 20:20:52 +0000

    Tue, Jun 17, 2025, 3:20 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,5 @@
 def main():
-    fraction = input("Fraction: ")
-    print(get_fraction(fraction))
+    print(get_fraction())
 
 
 # create a loop if user input is not a fraction
@@ -9,7 +8,7 @@ def get_fraction(fraction):
         try:
             # ask user for the fraction
 
-            #fraction = input("Fraction:" )
+            fraction = input("Fraction:" )
 
             #declare the parts of the fraction
             numer, deno = fraction.split("/")
