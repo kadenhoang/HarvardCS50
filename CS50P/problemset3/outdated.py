a566970d18c051d07d5961ea0020ff11359c1c4b
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-18 21:49:57 +0000

    Wed, Jun 18, 2025, 4:49 PM -05:00

--- CS50P/problemset3/outdated.py
+++ CS50P/problemset3/outdated.py
@@ -15,16 +15,17 @@ months = [
 
 
 while True:
-    try:
-        date = intput("Date:")
-        m,d,y = date.split("/")
-        m = int(m)
-        d = int(d)
-        y = int(y)
-        if (d >= 1 and d <= 31) and (m >= 1 and m <= 12):
-            break
-
-    except:
+    date = intput("Date:")
         try:
-            old_m,old_d,old_y = date.split("")
+
+            m,d,y = date.split("/")
+            m = int(m)
+            d = int(d)
+            y = int(y)
+            if (d >= 1 and d <= 31) and (m >= 1 and m <= 12):
+                break
+
+        except:
+            try:
+                old_m,old_d,old_y = date.split(" ")
 
