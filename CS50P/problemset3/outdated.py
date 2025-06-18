7eda3494d271ee2f36116ef64a2151ebac2252a0
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-18 22:11:41 +0000

    Wed, Jun 18, 2025, 5:11 PM -05:00

--- CS50P/problemset3/outdated.py
+++ CS50P/problemset3/outdated.py
@@ -16,27 +16,27 @@ m = [
 
 while True:
     date = input("Date:")
-        try:
+    try:
 
-            m,d,y = date.split("/")
-            m = int(m)
-            d = int(d)
-            y = int(y)
-            if (d >= 1 and d <= 31) and (m >= 1 and m <= 12):
-                break
+        m,d,y = date.split("/")
+        m = int(m)
+        d = int(d)
+        y = int(y)
+        if (d >= 1 and d <= 31) and (m >= 1 and m <= 12):
+            break
+
+    except:
+        try:
+            old_m,old_d,y = date.split(" ")
+            old_m = int(old_m)
+            old_d = int(old_d)
+            for i in range(len(m)):
+                if old_m == m[i]
+                    m = i + 1
+            d = old_d.replace(",","")
 
         except:
-            try:
-                old_m,old_d,y = date.split(" ")
-                old_m = int(old_m)
-                old_d = int(old_d)
-                for i in range(len(m)):
-                    if old_m == m[i]
-                        m = i + 1
-                d = old_d.replace(",","")
-
-            except:
-                pass
+            pass
 
 print(f"{y}-{m:02}-{d:02}")
 
