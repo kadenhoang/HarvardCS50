930161823e0e0a1ad06bc94c786adf4b0a868b5f
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-18 22:41:13 +0000

    Wed, Jun 18, 2025, 5:41 PM -05:00

--- CS50P/problemset3/outdated/outdated.py
+++ CS50P/problemset3/outdated/outdated.py
@@ -15,20 +15,26 @@ monthslist = [
 
 
 while True:
+    #ask the user for input
     date = input("Date:").strip()
     try:
+        #split the date input in to 3 part
         month, day, year = date.split("/")
         month = int(month)
         day = int(day)
         year = int(year)
+        #check if the date and month is within correct range
         if 1 <= day <= 31 and 1 <= month <= 12:
             break
     except:
         try:
+            # split the original input again with " "
             old_m, old_d, year = date.split(" ")
             old_d = old_d.replace(",", "")
             day = int(old_d)
             year = int(year)
+
+            # if the month match with the indice of the list
             for i in range(len(monthslist)):
                 if old_m == monthslist[i]:
                     month = i + 1
