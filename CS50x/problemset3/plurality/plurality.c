436bf4f9afdcadd777c1e01f6575233de7b120bd
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-27 22:30:22 +0000

    Fri, Jun 27, 2025, 5:30 PM -05:00

--- CS50x/problemset3/plurality/plurality.c
+++ CS50x/problemset3/plurality/plurality.c
@@ -80,6 +80,9 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    //the maximum number of voters
+    
+
+    //print the candidate with maximum votes
     return;
 }
