bc934fda608bf0cfea853e2acb748ff6d3a1796a
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-06-30 22:45:33 +0000

    Mon, Jun 30, 2025, 5:45 PM -05:00

--- CS50x/problemset3/runoff/runoff.c
+++ CS50x/problemset3/runoff/runoff.c
@@ -141,15 +141,15 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int j = 0; j < candidate_count; j ++)
+    for (int i = 0; i < voter_count; i ++)
     {
-        for (int i = 0; i < voter_count; i ++)
+        for (int j = 0; j < candidate_count; j ++)
         {
-            if (strcmp(candidates[i].name,name) == 0 )
+            
             {
                 candidates[i]votes += 1
             }
-            
+
         }
 
     }
