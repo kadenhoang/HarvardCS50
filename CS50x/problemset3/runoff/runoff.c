69576bc38e4cf025c1d453e0d52d208b20541523
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-02 22:06:07 +0000

    Wed, Jul 2, 2025, 5:06 PM -05:00

--- CS50x/problemset3/runoff/runoff.c
+++ CS50x/problemset3/runoff/runoff.c
@@ -222,6 +222,12 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+            {
+                return true;
+            }
+    }
     return;
 }
