cafc74786e6e89338c8ed31ae21513d6d804635c
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-02 21:54:56 +0000

    Wed, Jul 2, 2025, 4:54 PM -05:00

--- CS50x/problemset3/runoff/runoff.c
+++ CS50x/problemset3/runoff/runoff.c
@@ -197,7 +197,6 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    int min;
     for (int i = 0; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated)
