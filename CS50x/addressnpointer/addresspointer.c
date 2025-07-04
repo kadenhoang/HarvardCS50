5706cb611e699ba9dcb99616695b26273a25195e
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-04 22:46:02 +0000

    Fri, Jul 4, 2025, 5:46 PM -05:00

--- CS50x/addressnpointer/addresspointer.c
+++ CS50x/addressnpointer/addresspointer.c
@@ -8,8 +8,11 @@ int main(void)
 // * = pointer that get the value in the address
 //& = show address of smt
 
+    //print the string
+    printf("%s\n",n)
+    //print address of n string
     printf("%p\n", n );
-    //print the address of n
+    //print the address of n char
     printf("%p\n", &n[0]);
     printf("%p\n", &n[1]);
     printf("%p\n", &n[2]);
