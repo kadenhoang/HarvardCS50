5e659bbe916fc11b3a4285dcbb12d018f2b37a28
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-14 21:45:55 +0000

    Mon, Jul 14, 2025, 4:45 PM -05:00

--- CS50x/volume/volume.c
+++ CS50x/volume/volume.c
@@ -47,7 +47,11 @@ int main(int argc, char *argv[])
     }
 
     // TODO: Read samples from input file and write updated data to output file
+    fread(&buffer,sizeof(int16_t),1,input);
 
+    buffer *= factor;
+
+    fwrite(&buffer, sizeof(int16_t),1,output);
     // Close files
     fclose(input);
     fclose(output);
