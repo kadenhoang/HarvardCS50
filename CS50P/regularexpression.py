a3c4bbf9c92726a14b0d38fd26d82ca409af4132
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-14 19:55:31 +0000

    Mon, Jul 14, 2025, 2:55 PM -05:00

--- CS50P/regularexpression.py
+++ CS50P/regularexpression.py
@@ -10,13 +10,14 @@ email = input("what is your email?:").strip()
 # {m,n} = m to n repetiions
 # ^ = match the begining of the string(str start with what comes after ^)
 # & = match the end of the string(str ends after $)
-# r(raw) = treat the string as written. if \n = newline then r"\n" = output the "n"
 # [] = set of characters [a-zA-Z0-9_] = only allow this set of alphanumeric
 # [^] = any characters except smt. ex:[^@] = anything except @
 # \w = [a-zA-Z0-9_] = aplphanumeric (more friendly)
+# r(raw) = treat the string as written. if \n = newline then r"\n" = output the "n"
 if re.search(r"^[^@]+@[^@]+\.edu$", email):
         # (r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email)
         # (r"^\w+@\w+\.edu$", email)
+        # (r"^(\w\.)@(\w\.)?\w+\.edu$",email,re.IGNORECASE)
     print("Valid")
 else:
     print("Invalid")
