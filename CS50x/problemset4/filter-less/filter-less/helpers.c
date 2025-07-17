c09aa8eeabb419a83799b2181d598357dc6b1a67
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-17 21:40:38 +0000

    Thu, Jul 17, 2025, 4:40 PM -05:00

--- CS50x/problemset4/filter-less/filter-less/helpers.c
+++ CS50x/problemset4/filter-less/filter-less/helpers.c
@@ -28,7 +28,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // 
+            // Compute sepia values
+            sepiaRed =
 
             // Update pixel with sepia values
         }
