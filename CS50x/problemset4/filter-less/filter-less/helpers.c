45447df59c01a1193475b29a482631df0857a654
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-17 21:55:24 +0000

    Thu, Jul 17, 2025, 4:55 PM -05:00

--- CS50x/problemset4/filter-less/filter-less/helpers.c
+++ CS50x/problemset4/filter-less/filter-less/helpers.c
@@ -29,14 +29,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Compute sepia values
-            sepiaRed = (0.393 * image[i][j]rgbtRed) + (0.769 * image[i][j]rgbtGreen) + (0.189 * image[i][j]rgbtBlue);
-            sepiaGreen = (0.349 * image[i][j]rgbtRed) + (0.686 * image[i][j]rgbtGreen) + (0.168 * image[i][j]rgbtBlue);
-            sepiaBlue =  (0.272 * image[i][j]rgbtRed) + (0.534 * image[i][j]rgbtGreen) + (0.131 * image[i][j]rgbtBlue);
+
+            int sepiaRed = round((0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue));
+            int sepiaGreen = round((0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue));
+            int sepiaBlue =  round((0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue));
+
 
             // Update pixel with sepia values
             image[i][j]rgbtRed = sepiaRed;
             image[i][j]rgbtGreen = sepiaGreen;
             image[i][j]rgbtBlue = sepiaBlue;
+
+            if (image[i][j]rgbtRed,image[i][j]rgbtGreen,image[i][j]rgbtBlue) > 225
+            {
+                
+            }
         }
 
     return;
