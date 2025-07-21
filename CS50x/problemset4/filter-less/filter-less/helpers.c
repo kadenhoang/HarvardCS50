df045174f5ebe8799c2d2db5b4f02381705f3d93
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-21 22:41:30 +0000

    Mon, Jul 21, 2025, 5:41 PM -05:00

--- CS50x/problemset4/filter-less/filter-less/helpers.c
+++ CS50x/problemset4/filter-less/filter-less/helpers.c
@@ -68,7 +68,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i ++)
     {
-        for(int j = 0; j < width; j++)
+        for(int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - 1 - j];
