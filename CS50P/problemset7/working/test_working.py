b25d4face697c92f30e8a43543936144e7e73f35
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-21 21:43:56 +0000

    Mon, Jul 21, 2025, 4:43 PM -05:00

--- CS50P/problemset7/working/test_working.py
+++ CS50P/problemset7/working/test_working.py
@@ -1,4 +1,13 @@
 from working import convert
 
-def test_valid():
-    assert convert("9 AM to 9 PM") == "9:00 to 21:00"
+def test_simple_time():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+
+def test_with_minutes():
+    assert convert("9:30 AM to 5:15 PM") == "09:30 to 17:15"
+
+def test_midnight_to_noon():
+    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
+
+def test_noon_to_midnight():
+    assert convert("12 PM to 12 AM") == "12:00 to 00:00"
