b64a525e3166820ae6013892f5214bfbbdc5ccce
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-22 20:39:31 +0000

    Tue, Jul 22, 2025, 3:39 PM -05:00

---
+++ CS50P/object-oriented/dict.py
@@ -0,0 +1,15 @@
+def main()
+    student = get_student()
+    print(f"{student['name']} from {student['house']})
+
+def get_student():
+    student = {}
+    student['name'] = input("Name:")
+    student['house'] = input("House:")
+
+    # or
+
+    # name = input('Name:')
+    # house = input('House:')
+    # return {'name': name, 'house' : house}
+    return student
