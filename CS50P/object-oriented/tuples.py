bd8ea0628be4bbf4ce6e500ef3fb34e48cb41859
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-22 20:33:19 +0000

    Tue, Jul 22, 2025, 3:33 PM -05:00

--- CS50P/object-oriented/tuples.py
+++ CS50P/object-oriented/tuples.py
@@ -7,6 +7,7 @@ def get_student():
     house = input("House:")
     # tuples use ()
     # the data inside cannot be changed (immuatable)
+    # use if I know the data should not be change
     return (name, house)
 
 if __name__ == "__main__":
