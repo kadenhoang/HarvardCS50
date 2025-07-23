37e386c808845410b026fabd615c24f14303616e
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-23 20:27:18 +0000

    Wed, Jul 23, 2025, 3:27 PM -05:00

--- CS50x/problemset4/recover/recover.c
+++ CS50x/problemset4/recover/recover.c
@@ -12,9 +12,14 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[512];
     FILE *card = fopen(argv[1], "r");
-    
+     //if the image cannot be open,inform
+    if (card = NULL)
+    {
+        printf("Could not open file.\n")
+
  return 1
- //if the image cannot be open,inform
+    }
+    
  return 1
 
  //file name = 3 digit decimal number start with 000
