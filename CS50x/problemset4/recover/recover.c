49e3986bdf92abff100c7811af80fb422020964d
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-23 20:57:59 +0000

    Wed, Jul 23, 2025, 3:57 PM -05:00

--- CS50x/problemset4/recover/recover.c
+++ CS50x/problemset4/recover/recover.c
@@ -12,7 +12,9 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[512];
     FILE *img = NULL;
-    char filename[]
+    char filename[8]
+    int file_count = 0;
+
     FILE *card = fopen(argv[1], "r");
      //if the image cannot be open,inform
     if (card = NULL)
@@ -20,12 +22,32 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n")
         return 1
     }
-    while(fread(&buffer,1,512,card) == 512)
+    while(fread(buffer,1,512,card) == 512)
     {
-        FILE *img = fopen(argv[1], "w")
-        fwrite(&img,sizeof(data),1,)
-    }
+        if (buffer[0] == 0xff &&
+            buffer[1] == 0xd8 &&
+            buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
 
-    // create JPEGS from the data
- sprintf(img, "%03i.jpg", 2);
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+             sprintf(filename, "%03i.jpg", file_count++);
+             img = fopen(filename,"w");
+        }
+
+        if (img != NULL)
+        {
+            fwrite(buffer,1,512,img);
+        }
+    }
+    if ( img != NULL)
+    {
+        fclose(img);
+    }
+    fclose(card);
+
+    return 0;
 }
