5fca041b9170d2667c213796c23fc61afd0b7245
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-23 20:20:40 +0000

    Wed, Jul 23, 2025, 3:20 PM -05:00

--- CS50x/problemset4/recover/recover.c
+++ CS50x/problemset4/recover/recover.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ //accept only 1 command-line, else remind
+    if (argv 1= 2)
+    {
+        prinf("Usage: ./recover FILE\n");
+        return 1
+    }
+
+    FILE *card = fopen(argv[1], "r");
+    
+ return 1
+ //if the image cannot be open,inform
+ return 1
+
+ //file name = 3 digit decimal number start with 000
 }
