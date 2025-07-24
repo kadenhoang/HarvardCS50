40b30b41008891af5aeb828100ab0248c6154256
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-24 22:43:58 +0000

    Thu, Jul 24, 2025, 5:43 PM -05:00

--- CS50x/resizing.c
+++ CS50x/resizing.c
@@ -3,15 +3,38 @@
 int main(void)
 {
 
-    int *list = malloc(3, sizeof(int));
+    int *list = malloc(3 * sizeof(int));
     if (list = NULL)
     {
         return 1;
     }
 
 
-        list[0] = 1;
-        list[1] = 2;
-        list[2] = 3;
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    
+    int *tmp = realloc(4 * sizeof(int))
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for(int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4
+
+    //free up data to prevent overflow or leak
+    free(list);
+
+    list = tmp;
+
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
     }
 }
