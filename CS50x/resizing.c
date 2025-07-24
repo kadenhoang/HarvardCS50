74d3ce374b4074302bae16ce34f4fedf7e4f29d0
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-24 22:34:45 +0000

    Thu, Jul 24, 2025, 5:34 PM -05:00

--- CS50x/resizing.c
+++ CS50x/resizing.c
@@ -2,5 +2,16 @@
 
 int main(void)
 {
-    int list[] = malloc(3, sizeof(list))
+    int list[] = malloc(3, sizeof(list));
+    if (list = NULL)
+    {
+        return 1;
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        list[0] = 1;
+        list[1] = 2;
+        list[2] = 3;
+    }
 }
