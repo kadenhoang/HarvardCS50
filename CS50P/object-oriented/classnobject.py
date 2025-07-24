74e182f506a0061d266a919f7f27ea1b25fab580
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-24 22:26:34 +0000

    Thu, Jul 24, 2025, 5:26 PM -05:00

--- CS50P/object-oriented/classnobject.py
+++ CS50P/object-oriented/classnobject.py
@@ -1,21 +1,45 @@
 class Student:
-    #initialize a function for the class
+    #a function in a class is called Method
+
+    #initialize a function and variables for the class
     def __init__(self,name,house):
     #make a third variable "self" to store name,house
+        if not name:
+            raise ValueError("Missing Name")
         self.name = name
         self.house = house
 
+    #self is refer to the object being created and has the varialbes
+
+    def __str__(self):
+        return (f"{self.name} from {self.house}")
+
+    #function and variable must have different name
+    @property #function getter
+    def house(self):
+        return self._house
+
+    @house.setter #function setter
+    def house(self,house):
+        if not house in ["Gryffindor","Ravenclaw","Slytherin","Hufflepuff"]:
+            raise ValueError("Invalid House")
+        self._house = house
+
 def main():
     student = get_student()
-    #student now is a object, to call an object: use student.variable
-    print(f"{student.name} from {student.house}")
+    #people can still override house in main so use getter, setter to protect it
+    student.house = "Nha Que"
+    #student now is a object, to call an object manually: use student.variable
+    #or use __str__
+    print(student)
+
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
     #create an object from class Student
-    student = Student(name,house)
-    return student
+    return Student(name,house)
+
 
 if __name__ == "__main__":
     main()
