73fdf0ada114986e47e8902ea54ca47069876e32
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-28 19:34:11 +0000

    Mon, Jul 28, 2025, 2:34 PM -05:00

--- CS50P/object-oriented/classnobject2.py
+++ CS50P/object-oriented/classnobject2.py
@@ -4,10 +4,22 @@ class Student:
         self.house = house
 
     def __str__(self):
-        prinf(f"{name} is in {house}")
+        print(f"{self.name} is in {self.house}")
 
     @classmethod #use the method wt creating an object
-    
+    def get(cls):
+        # 'cls' refer to the class,
+        # input now can be used within the class
+        name = input("Name:")
+        house = input("House:")
+        return cls(name,house)
+
+def main():
+    student = Student()
+    print(student)
+
+def __main__ == __
+
 
 
 
