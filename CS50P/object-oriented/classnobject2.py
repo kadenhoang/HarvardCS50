0af0de27dcbed70a6967928ddf07eada77bd9cc0
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-28 19:29:48 +0000

    Mon, Jul 28, 2025, 2:29 PM -05:00

---
+++ CS50P/object-oriented/classnobject2.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self,name,house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        prinf(f"{name} is in {house}")
+
+    @classmethod #use the method wt creating an object
+    
+
+
+
+
