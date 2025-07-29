99ab12d91051f26e3caf097f8a7bbbd9a636b65b
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-29 23:35:11 +0000

    Tue, Jul 29, 2025, 6:35 PM -05:00

--- CS50P/problemset8/seasons/test_seasons.py
+++ CS50P/problemset8/seasons/test_seasons.py
@@ -1,3 +1,9 @@
 from seasons import check_birthday
 
-def test_
+
+def test_birthday():
+    assert check_birthday("June 7,1995") == None
+    assert check_birthday("1995-01-01") == ("1995","01","01")
+    assert check_birthday("1995-1-1") == None
+
+
