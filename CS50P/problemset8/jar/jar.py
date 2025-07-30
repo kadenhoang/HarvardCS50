14abdc63fe9c254f0693cbb92f1aa367989befcb
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-30 19:31:05 +0000

    Wed, Jul 30, 2025, 2:31 PM -05:00

--- CS50P/problemset8/jar/jar.py
+++ CS50P/problemset8/jar/jar.py
@@ -1,20 +1,27 @@
 class Jar:
     def __init__(self, capacity=12):
+        if not capacity >= 0:
+            raise ValueError
         ...
 
     def __str__(self):
-        ...
+        ...return f"
 
     def deposit(self, n):
-        ...
+        if n is > 12:
+            raise ValueError
+        self.n = n
 
     def withdraw(self, n):
-        ...
+        if not n:
+            raise ValueError
 
     @property
     def capacity(self):
-        ...
+        self
 
     @property
     def size(self):
         ...
+
+def main():
