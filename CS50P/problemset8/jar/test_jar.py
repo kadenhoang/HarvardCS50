5aff9cb3ebba172e71442e6590b44dac09002ca0
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-30 20:52:53 +0000

    Wed, Jul 30, 2025, 3:52 PM -05:00

--- CS50P/problemset8/jar/test_jar.py
+++ CS50P/problemset8/jar/test_jar.py
@@ -2,5 +2,11 @@ from jar import Jar
 
 def test_withdraw():
     jar = Jar()
-    assert jar.withdraw(-1) == ValueError
-    assert jar.withdraw(1) 
+    jar.deposit(5)
+    jar.withdraw(2)
+    assert jar.size == 3
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(5)
+    assert jar.size == 5
