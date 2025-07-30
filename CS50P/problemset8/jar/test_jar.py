0a3da8dc90c830dfff17591cb22798676f59c742
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-30 20:59:59 +0000

    Wed, Jul 30, 2025, 3:59 PM -05:00

--- CS50P/problemset8/jar/test_jar.py
+++ CS50P/problemset8/jar/test_jar.py
@@ -10,3 +10,23 @@ def test_deposit():
     jar = Jar()
     jar.deposit(5)
     assert jar.size == 5
+
+def test_invalid_withdraw():
+    jar = Jar()
+    jar.deposit(5)
+    try:
+        jar.withdraw(10)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+def test_invalid_deposit():
+    jar = Jar()
+    try:
+        jar.deposit(100)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
