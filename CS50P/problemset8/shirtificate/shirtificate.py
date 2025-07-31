10c5eb9ec268ff8de880bcb1d9577d4241211525
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-31 19:31:48 +0000

    Thu, Jul 31, 2025, 2:31 PM -05:00

--- CS50P/problemset8/shirtificate/shirtificate.py
+++ CS50P/problemset8/shirtificate/shirtificate.py
@@ -1,5 +1,5 @@
 from fpdf import FPDF
-from PIL import Image
+
 
 pdf = FPDF()
 pdf.add_page()
@@ -7,5 +7,4 @@ pdf.set_font('Times', style='I', size = 16)
 pdf.cell(0, 10, text="CS50 Shirtificate", border=0, ln=1, align="C")
 pdf.image("shirtificate.png")
 
-shirt = Image.open("shirtificate.png")
-shirtsize = shirt.size
+
