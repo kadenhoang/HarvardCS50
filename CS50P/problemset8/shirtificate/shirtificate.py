9bfa5db86f5d200795b99efd1a84f5467797e394
kadenhoang <hoanggiaanhvu95@gmail.com>
2025-07-31 19:36:08 +0000

    Thu, Jul 31, 2025, 2:36 PM -05:00

--- CS50P/problemset8/shirtificate/shirtificate.py
+++ CS50P/problemset8/shirtificate/shirtificate.py
@@ -5,6 +5,10 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_font('Times', style='I', size = 16)
 pdf.cell(0, 10, text="CS50 Shirtificate", border=0, ln=1, align="C")
-pdf.image("shirtificate.png")
 
+shirt_width = 150
+page_width = pdf.w
+x = (page_width - shirt_width)/ 2
+
+pdf.image("shirtificate.png",x=x, y = 20, w=shirt_width)
 
